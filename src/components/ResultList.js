36f06c1ee317fd06bff76dfd1496c7a63a718a98
asadaziz95 <asadazizjs@gmail.com>
2019-12-02 15:53:46 +0500

    reslut list component initaited

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -4,11 +4,16 @@ import {View, Text, StyleSheet} from 'react-native';
 const ResultsList = ({title}) => {
   return (
     <View>
-      <Text style={}>{title}</Text>
+      <Text style={styles.headerStyling}>{title}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headerStyling: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
 
 export default ResultsList;
