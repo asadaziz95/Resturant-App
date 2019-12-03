d2410eea09ae695a599f1073c2a5a37cc6a12718
asadaziz95 <asadazizjs@gmail.com>
2019-12-03 16:58:16 +0500

    resturant app completed

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, FlatList, Image,StyleSheet} from 'react-native';
+import {Text, View, FlatList, Image, StyleSheet} from 'react-native';
 import yelp from '../api/yelp.js';
 
 const ResultShowScreen = ({navigation}) => {
@@ -19,24 +19,41 @@ const ResultShowScreen = ({navigation}) => {
   }
   return (
     <View>
-      <Text>{result.name}</Text>
       <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
         data={result.photos}
         keyExtractor={photo => photo}
         renderItem={({item}) => {
           return <Image style={styles.image} source={{uri: item}} />;
         }}
       />
+      <View style={styles.container}>
+        <Text style={styles.title}>{result.name}</Text>
+        
+        <Text>{`Contact Number:${result.display_phone}`}</Text>
+        <Text>{`City:${result.location.city}`}</Text>
+      <Text>{`Address:${result.location.address1}`}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  image:{
-    height:200,
-    width:300,
-    borderRadius:5
-  }
-})
+  image: {
+    height: 200,
+    width: 430,
+    borderRadius: 2,
+  },
+  container: {
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  title: {
+    fontSize: 22,
+    color: '#05a5d1',
+    fontWeight: 'bold',
+  },
+});
 
 export default ResultShowScreen;
