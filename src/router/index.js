0dbfd99162fc2592106c389655775f2fa127d6f5
asadaziz95 <asadazizjs@gmail.com>
2019-12-03 12:11:49 +0500

    Result show screen component added

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import SearchScreen from './../screens/SearchScreen.js';
+import ResultShowScreen from './../screens/ResultShowScreen.js';
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
+    ResultShow: ResultShowScreen,
   },
   {
     initialRouteName: 'Search',
---
+++ src/screens/ResultShowScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ResultShowScreen = () => {
+  return (
+    <View>
+      <Text>Result Show Screen</Text>
+    </View>
+  );
+};
+
+export default ResultShowScreen;
